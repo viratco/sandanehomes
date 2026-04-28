c90ca415d89998c28087586b3b7401c0867a1d2a
viratco <viratvaroniya31@gmail.com>
2026-04-28 20:55:18 +0530

    Add floating back button to all subpages via SubpageNav component

--- src/components/SubpageNav.jsx
+++ src/components/SubpageNav.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 import './SubpageNav.css';
 
 const SubpageNav = () => {
+    const navigate = useNavigate();
+
     return (
-        <div className="subpage-nav">
-            {/* Subpage Navigation */}
+        <div className="subpage-nav-container">
+            <button className="subpage-back-button" onClick={() => navigate(-1)}>
+                <FaArrowLeft />
+                <span>Back</span>
+            </button>
         </div>
     );
 };
