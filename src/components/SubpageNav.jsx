23ada4f53031fa8c827dfab090e7359f473c091e
viratco <viratvaroniya31@gmail.com>
2026-04-29 17:05:12 +0530

    Remove back button from all subpages

--- src/components/SubpageNav.jsx
+++ src/components/SubpageNav.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
 import './SubpageNav.css';
 
 const SubpageNav = () => {
-    const navigate = useNavigate();
-
-    return (
-        <div className="subpage-nav-container">
-            <button className="subpage-back-button" onClick={() => navigate(-1)}>
-                BACK
-            </button>
-        </div>
-    );
+    return null; // Removed back button as requested
 };
 
 export default SubpageNav;
